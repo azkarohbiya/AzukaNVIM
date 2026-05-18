e09aab94fc2f7bd640a559245b6630c62ae891b7
azuka31 <azkarohbiya@gmail.com>
2026-05-18 10:34:47 +0700

    config mq5

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use a custom filetype so no treesitter parser is attempted (cpp parser crashes on MQL5 syntax)
+-- then apply cpp regex syntax for highlighting
+vim.filetype.add({
+  extension = {
+    mq5 = "mql5",
+    mq4 = "mql5",
+    mqh = "mql5",
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "mql5",
+  callback = function()
+    vim.bo.syntax = "cpp"
+  end,
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,4 +14,16 @@ return {
             "sql",
         })
     end,
+    init = function()
+        -- nvim-treesitter master branch passes Lua table nodes to vim.treesitter.get_range,
+        -- but Neovim 0.12.2 calls node:range(true) on them, crashing because the method is absent.
+        -- Guard against nil/missing range method until nvim-treesitter is updated.
+        local orig = vim.treesitter.get_range
+        vim.treesitter.get_range = function(node, source, metadata)
+            if not node or type(node.range) ~= "function" then
+                return { 0, 0, 0, 0, 0, 0 }
+            end
+            return orig(node, source, metadata)
+        end
+    end,
 }
